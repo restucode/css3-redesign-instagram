e6a06204a7d7d3624c6e6a665a16451710135059
Restu <restu.kersana1@gmail.com>
2020-12-24 14:54:04 +0700

    'fix-dekstop'

--- js/main.js
+++ js/main.js
@@ -7,7 +7,12 @@ var swiperStories = new Swiper('.swiper-stories', {
 const sidebar = document.querySelector('.sidebar')
 const bottomShowMenu = document.querySelector('.bottom-show-menu')
 const closeSidebar = document.querySelector('.close-sidebar')
-// event
+const closeSidebarDekstop = document.querySelector('.close-sidebar-dekstop')
+const bottomNav = document.querySelector('.bottom-nav')
+const stories = document.querySelector('.stories')
+const feed = document.querySelector('.feed')
+
+// event mobile
 bottomShowMenu.addEventListener('click', () => {
   sidebar.classList.add('active')
   document.body.style.overflowY = 'hidden';
@@ -18,14 +23,31 @@ closeSidebar.addEventListener('click', () => {
  document.body.style.overflowY = 'scroll';
 })
 
+// event desktop
+closeSidebarDekstop.addEventListener('click', function() {
+ this.classList.toggle('active-desktop')
+ sidebar.classList.toggle('active-desktop')
+ bottomNav.classList.toggle('active-desktop')
+ feed.classList.toggle('active-desktop')
+ stories.classList.toggle('active-desktop')
+})
+
+
 window.addEventListener('scroll', () => {
- let footer = document.querySelector('#footer');
- const bottomNav = document.querySelector('.bottom-nav');
+
  const scrollable = document.documentElement.scrollHeight - window.innerHeight;
  if(window.scrollY === scrollable) {
   bottomNav.classList.add('active'); 
  } else {
   bottomNav.classList.remove('active'); 
+ }
+
+})
+
+window.addEventListener('resize', function() {
+ console.log(window.innerWidth)
+ if(window.innerWidth >= 1024) {
+   document.body.style.overflowY = 'scroll'
  }
 
 })
